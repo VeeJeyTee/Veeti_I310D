adf3af3e2cd74d1fbffd5fbacfc723db24bc2bc4
VeeJeyTee <158378290+VeeJeyTee@users.noreply.github.com>
2024-04-12 18:38:30 -0500

    Update volume_of_sphere.py
    Improve code and add both radii to be calculated.

--- volume_of_sphere.py
+++ volume_of_sphere.py
@@ -1,7 +1,13 @@
-def calculate_volume_of_sphere(radius)
+def calculate_volume_of_sphere(radius):
+  '''Calculate the volume of a sphere.'''
   pi = 3.14
   volume = 4/3 * pi * (radius)**3
+  return volume
 
-radius1 = 30
-volume1 = calculate_volume_of_sphere(radius1)
-print(f"The volume of a sphere with a radius of {radius1} would be {volume1}")
+radius_1 = 30
+volume_1 = calculate_volume_of_sphere(radius_1)
+print(f"The volume of a sphere with a radius of {radius_1} is: {volume_1}")
+
+radius_2 = 40 
+volume_2 = calculate_volume_of_sphere(radius_2)
+print(f"The volume of a sphere with a radius of {radius_2} is: {volume_2}")
